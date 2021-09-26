64b56b428af7ecc6403319ba3b352c665b6c2140
3020244131-Ji Xiangyu <luobojiaozi@163.com>
2021-09-26 16:53:08 +0800

    feat:create camera pool

--- C/include/device/camera_pool.h
+++ C/include/device/camera_pool.h
@@ -1,6 +1,6 @@
 /**
  * @file C/include/device/pool.h
- * @brief      This file defines camera management.
+ * @brief      This file defines camera pool.
  * @version    0.0.2
  * @author     Ji Xiangyu
  * @date       2021.9.25
@@ -8,6 +8,16 @@
 #include "common.h"
 #include <pthread.h>
 
+/**
+ * @defgroup   Camera_Pool
+ * @brief      camera pool for camera cluster
+ * @{
+ */
+/**
+ * @defgroup   Camera_Pool_State
+ * @brief      camera's state and status
+ * @{
+ */
 enum{
     MAIN=0,
     BACKUP=1,
@@ -19,25 +29,64 @@ enum{
     WAITING=129,
     DISCONNECTED=130
 };
-
+/**
+ * @}
+ */
+/**
+ * @defgroup   Camera_Pool_Type
+ * @brief      definition of type
+ * @{
+ */
+/**
+ * @typedef Camera
+ * @struct Camera
+ * @brief      wrapper of physical camera
+ * @{
+ */
 typedef struct{
-    char* xaddr;
-    int catelogy;
-    int state;
-    int disconnection;
+    char* xaddr;/*!< camera's media service address */
+    int catelogy;/*!< camera's status */
+    int state;/*!< camera's state */
+    int disconnection;/*!< camera's times of disconnection */
 } Camera;
+/**
+ * @}
+ */
 
+/**
+ * @typedef CameraPool
+ * @struct CameraPool
+ * @brief  definition of camera pool    
+ * @{
+ */
 typedef struct{
-    uint64_t timestamp;
-    Camera* cameras;
-    int size;
-    int main_num;
-    int backup_num;
-    pthread_mutex_t lock;
-    int max_timeout;
-    int heart;
+    uint64_t timestamp;/*!< detection timeout */
+    Camera* cameras;/*!< cameras array */
+    int size;/*!< cameras number */
+    int main_num;/*!< main cameras number */
+    int backup_num;/*!< backup cameras number */
+    pthread_mutex_t lock;/*!< thread lock */
+    int max_timeout;/*!< maximum acceptable times of disconnection */
+    int heart;/*!< weather to use heartbeat */
 } CaremaPool;
+/**
+ * @}
+ */
+/**
+ * @}
+ */
 
-CaremaPool* createCameraPool(int main_num, int backup_num);
+/**
+ * @defgroup   Camera_Pool_Exported_Methods
+ * @brief      exported methods of camera pool
+ * @{
+ */
+CaremaPool* createCameraPool(int, int, int);
 void heartbeat(CaremaPool*);
 void stopHeartbeat(CaremaPool*);
+/**
+ * @}
+ */
+/**
+ * @}
+ */
--- C/src/device/camera_pool.c
+++ C/src/device/camera_pool.c
@@ -1,3 +1,10 @@
+/**
+ * @file C/src/device/camera_pool.c
+ * @brief      This file implements camera pool.
+ * @version    0.0.2
+ * @author     Ji Xiangyu
+ * @date       2021.9.26
+ */
 #include "device/pool.h"
 
 static void update_cameras(CaremaPool*);
@@ -6,10 +13,19 @@ static void update_cameras(CaremaPool*);
  * @brief      Creates a camera pool.
  * @param[int]  main_num：The main camera number.
  * @param[int]  backup_num:The backup camera number.
+ * @param[int]  max_timeout:The maximum times of disconnection.
  * @return     CameraPool* Camera pool
  */
-CaremaPool* createCameraPool(int main_num, int backup_num){
-    return NULL;
+CaremaPool* createCameraPool(int main_num, int backup_num, int max_timeout){
+    CameraPool* pool = (CaremaPool*)malloc(sizeof(CaremaPool));
+    pool -> timestamp = 0;
+    pool -> main_num = main_num;
+    pool -> backup_num = backup_num;
+    pthread_mutex_init(&(pool -> lock), NULL);
+    pool -> max_timeout = max_timeout;
+    pool -> heart = 1;
+    update_cameras(pool);
+    return pool;
 }
 /**
  * @brief      start heartbeat request
