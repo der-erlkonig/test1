aadfe9bd85f9de7b2b5aba9f3ce3dab30b415ed1
changchun6607 <gantang103@163.com>
2021-10-19 14:22:12 +0800

    test:pull request

--- C/include/network/network.h
+++ C/include/network/network.h
@@ -5,6 +5,7 @@
  * @author     Ji Xiangyu
  * @date       2021.10.19
  */
+
 #pragma once
 #include "lib.h"
 #include <arpa/inet.h>
@@ -12,6 +13,5 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <sys/types.h>
-
 
 int getLocalAddress(char**);
